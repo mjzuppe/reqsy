9a111a0b1e8b32dbfd2ba91fc8fafb5c30a7c853
mark zuppe <zuppe@live.com>
2023-10-10 21:29:42 -0400

    updated custom component for input-mention -- placeholder, value, and tag detection

--- src/app/util/ui/input-mention.tsx
+++ src/app/util/ui/input-mention.tsx
@@ -1,42 +1,45 @@
-import React, { ReactNode, useRef, useState } from 'react';
-/// create div with contentEditable
-/// create placeholder
-/// create dropdown with options below div
-/// create basic search filter for dropdown
-/// listen to input (onInput (e) e.currenttarget.textContent)
-/// when current chunk starts with '@' display dropdown if valid options exists
-/// if an option is selected, replace current chunk with option
-/// listen to input and if chunk with @ is valid, style with <span/>
+import React, { useRef, useState } from 'react';
 
-const InputMention = (props: { placeholder?: string, style?: any }) => {
-    const { placeholder, ...rest } = props;
-    const inputRef = useRef(null);
-    const [value, setValue] = useState("");
-    const [focus, setFocus] = useState(false);
-    const parseTags = (text: string) => !text ? "" : text.split(" ").map((chunk: string) => chunk[0] === "@" ? <span style={{ marginLeft: "5px", marginRight: "5px", color: "#ACDFFF" }}>{chunk}</span> : chunk );
-    const Placeholder = () => placeholder || "";
 
-    const handleFocus = (e: any) => setFocus(true);
-    const handleBlur = (e: any) => setFocus(false);
-    const handleInput = (e: any) => {
-        e.preventDefault();
+const parseTags = (text: string) => !text ? "" : text.split(" ").map((chunk: string) => chunk[0] === "@" ? <span style={{ marginLeft: "5px", marginRight: "5px", color: "#ACDFFF" }}>{chunk}</span> : chunk );
+
+const InputMention = (props: { defaultValue?: string, placeholder?: string, style?: any }) => {
+    const { defaultValue, placeholder, ...rest } = props;
+
+    const [value, setValue] = useState(defaultValue || null);
+    const [displayPlaceholder, setDisplayPlaceholder] = useState(Boolean(!value));
+    const [key, setKey ] = useState(0);
+    
+    const children = value;
+    
+    const handleRender = (e: any) => setKey(key + 1)    ;
+    const handleBlur = (e: any) => {
         setValue(e.currentTarget.textContent);
-    }
-    const handleClick = () => {
-        setFocus(true);
-        setTimeout(() => inputRef.current.focus(), 100);
-        
+        handleRender(e);
     }
 
-    const tempStyle = { width: "100px", height: "30px", lineHeight: "30px", paddingLeft: "1px" };
-    console.log("REST", rest)
+    const handleInput = (e: any) => {
+        if (e.currentTarget.textContent) setDisplayPlaceholder(false);
+        else if (!e.currentTarget.textContent) setDisplayPlaceholder(true);
+    };
+    const handleKeyDown = (e: any) => {
+        // allow delete no matter what
+        if (e.keyCode === 8) return;
+        // prevent enter from creating a new line
+        if (e.keyCode === 13) e.preventDefault();
+        // prevent tab from creating a new tab
+        if (e.keyCode === 9) e.preventDefault();
+        // prevent more then 24 characters 
+        if (e.currentTarget.textContent.length > 24) e.preventDefault();
+    }
+
+
     return (
-        <div {...rest} onClick={handleClick}>
-            <div style={tempStyle} className={focus? "items-border-bottom" : "hide"} ref={inputRef} onFocus={handleFocus} onBlur={handleBlur} onInput={handleInput} contentEditable/>
-            <div style={tempStyle} className={!focus? "items-border-bottom" : "hide"}>{!value? placeholder : parseTags(value)}</div>
-        </div>
-
-    )
-}
+    <div  style={{display: "grid", placeItems: "flex-start", gridTemplateAreas: "inner-div"}}>
+        {displayPlaceholder ? <div style={{padding: "1px", width: "130px",gridArea: "inner-div", color: "rgba(255, 255, 255, 0.4)"}}>{placeholder}</div> : ""}
+        <div onKeyDown={handleKeyDown} onInput={handleInput} style={{padding: "1px", width: "130px", gridArea: "inner-div", border: "1px solid red"}} className="input-mention" onBlur={handleBlur} key={key} contentEditable>{parseTags(children)}</div>
+    </div>
+    
+    )};
 
 export { InputMention }
