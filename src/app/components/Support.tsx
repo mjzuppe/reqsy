24811435dd859eec3b4a098ece5f5943a2ceb2fc
mark zuppe <zuppe@live.com>
2023-10-20 12:12:36 -0400

    settings > support UI implemented

--- src/app/components/Support.tsx
+++ src/app/components/Support.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Select } from "../util/ui/select";
+import { Button } from "figma-ui-kit";
 
-export const Support = (props: {user: any}) => {
-   return ( <h1>Support-Add Crisp</h1>)
+export const Support = (props: { user: any }) => {
+   return (
+      <div id="action-container">
+         <div className="action-container-content">
+            <div ><p><strong>Contact us.</strong> We want to hear from you.</p></div>
+         </div>
+         <div className="action-container-subcontainer">
+            <Select style={{marginTop: "10px"}}  id={"support-category"} defaultValue={null} placeholder="Subject" options={["General", "Bug", "Feature Request"].map((opt: string) => ({ label: opt, value: opt }))} />
+            <div style={{marginLeft: "5px", width: "97%", marginTop: "10px"}} ><input placeholder="Enter email address" className="text-input" type="text"/></div>
+            <div style={{  marginTop: "10px",  display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+               <textarea maxLength={256} className="support-text" placeholder="Enter your message..." />
+            </div>
+            <div style={{marginLeft: "10px"}}>
+            <Button secondary style={{ fontSize: "10px", height: "20px", lineHeight: "10px" }} >submit</Button>
+            </div>
+         </div>
+      </div>
+   )
 }
